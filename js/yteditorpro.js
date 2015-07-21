2919f3f1df16436c37d81399313ca9d484bc3aab
Kelly Davis <kldavis4@gmail.com>
2015-07-21 17:34:54 -0400

    Enable google analytics using http://stackoverflow.com/a/10371025/290918

--- js/yteditorpro.js
+++ js/yteditorpro.js
@@ -1,7 +1,48 @@
+var _gaq = _gaq || [];
+
 (function(){
+    var UPDATE_INTERVAL = 24 * 60 * 60 * 1000; // Update after 24 hours
+
     var active = true;
     var publishRequested = false;
-                    
+
+    _gaq.push(function() {
+        _gaq.push(['_setAccount', 'UA-10393243-10']);
+        _gaq.push(['_trackPageview']);
+        _gaq.push(['_trackEvent', 'Extension', 'GALoaded']);
+    });
+
+    // Retrieve GA from storage
+    chrome.storage.local.get({
+        lastUpdated: 0,
+        code: ''
+    }, function(items) {
+        if (Date.now() - items.lastUpdated > UPDATE_INTERVAL) {
+            // Get updated file, and if found, save it.
+            get('https://ssl.google-analytics.com/ga.js', function(code) {
+                if (!code) return;
+                chrome.storage.local.set({lastUpdated: Date.now(), code: code});
+            });
+        }
+        if (items.code) {// Cached GA is available, use it
+            eval_code(items.code);
+        } else {// No cached version yet. Load from extension
+            get(chrome.extension.getURL('js/ga.js'), eval_code);
+       }
+    });
+
+    // Typically run within a few milliseconds
+    function eval_code(code) {
+        try { window.eval(code); } catch (e) { console.error(e); }
+    }
+
+    function get(url, callback) {
+        var x = new XMLHttpRequest();
+        x.onload = x.onerror = function() { callback(x.responseText); };
+        x.open('GET', url);
+        x.send();
+    }
+                        
     $("#publish-button").click(function(evt) {
         publishRequested = true; //Leaving the page to publish video
     });
@@ -10,7 +51,10 @@
     function confirmExit(event) {
         //Don't request confirm if publishing
         if ( publishRequested ) {
+            _gaq.push(['_trackEvent', 'Extension', 'PublishExit']);
             return null;
+        } else {
+            _gaq.push(['_trackEvent', 'Extension', 'ConditionalExit']);
         }
         
         return "There may be unsaved changes. Do you wish to leave the page?";
@@ -85,14 +129,17 @@
     jQuery(document).keypress(
         function(event) {
             if ( event.which == 1 ) { //CTRL-A
+                _gaq.push(['_trackEvent', 'Hotkey', 'ToggleActive']);
                 toggleActive();
             }
 
             if ( active ) {
                 if ( event.which == 99 ) { //C - Close current window
+                    _gaq.push(['_trackEvent', 'Hotkey', 'CloseWindow']);
                     jQuery(".close-button")[0].click();
                     jQuery(".close-button")[1].click();
                 } else if ( event.which == 32 ) { //Spacebar - Pause/Play video
+                    _gaq.push(['_trackEvent', 'Hotkey', 'PausePlay']);
                     event.preventDefault();
                     var player = jQuery("#preview-swf")[0];
 
@@ -102,6 +149,7 @@
                         player.playVideo();
                     }
                 } else if ( event.which == 115 ) { //S - sort thumbnails
+                    _gaq.push(['_trackEvent', 'Hotkey', 'SortThumbnails']);
                     if ( jQuery("#images-tab").attr('class').indexOf('selected') != -1 ) {
                         var parent = jQuery(jQuery(".image-original")[0]).parent();
                         var images = jQuery(".image-original").detach();
@@ -115,8 +163,10 @@
                         images.appendTo(parent);
                     }
                 } else if ( event.which == 107) { //K - ken burns effect
+                    _gaq.push(['_trackEvent', 'Hotkey', 'KenBurns']);
                     jQuery("input[effectid*='KEN_BURNS']").click();
                 } else if ( event.which == 43) { //+ key increase by one second selected clip
+                    _gaq.push(['_trackEvent', 'Hotkey', 'Increase1S']);
                     jQuery(".right-trimmer").each(function(idx,val) {
                         var target = jQuery(val);
                         if (target.css("visibility") == "visible") {
@@ -126,6 +176,7 @@
                         }
                     });
                 } else if ( event.which == 45) { //- key decrease by one second selected clip
+                    _gaq.push(['_trackEvent', 'Hotkey', 'Decrease1S']);
                     jQuery(".right-trimmer").each(function(idx,val) {
                         var target = jQuery(val);
                         if (target.css("visibility") == "visible") {
@@ -135,50 +186,60 @@
                         }
                     });
                 } else if ( event.which == 49) { //1 - video tab
+                    _gaq.push(['_trackEvent', 'Hotkey', 'VideoTab']);
                     if (jQuery("#mediapicker-infobox").attr('class') == "infobox-visible") {
                         jQuery("#mediapicker-infobox").toggleClass("mediapicker-visible");
                         jQuery("#mediapicker-infobox").toggleClass("infobox-visible");
                     }
                     jQuery("#video-tab").click();
                 } else if ( event.which == 50) { //2 - copyright tab
+                    _gaq.push(['_trackEvent', 'Hotkey', 'CopyrightTab']);
                     if (jQuery("#mediapicker-infobox").attr('class') == "infobox-visible") {
                         jQuery("#mediapicker-infobox").toggleClass("mediapicker-visible");
                         jQuery("#mediapicker-infobox").toggleClass("infobox-visible");
                     }
                     jQuery("#cc-tab").click();                    
                 } else if ( event.which == 51) { //3 images tab
+                    _gaq.push(['_trackEvent', 'Hotkey', 'ImagesTab']);
                     if (jQuery("#mediapicker-infobox").attr('class') == "infobox-visible") {
                         jQuery("#mediapicker-infobox").toggleClass("mediapicker-visible");
                         jQuery("#mediapicker-infobox").toggleClass("infobox-visible");
                     }
                     jQuery("#images-tab").click();
                 } else if ( event.which == 52) { //4 audio tab
+                    _gaq.push(['_trackEvent', 'Hotkey', 'AudioTab']);
                     if (jQuery("#mediapicker-infobox").attr('class') == "infobox-visible") {
                         jQuery("#mediapicker-infobox").toggleClass("mediapicker-visible");
                         jQuery("#mediapicker-infobox").toggleClass("infobox-visible");
                     }
                     jQuery("#audio-tab").click();
                 } else if ( event.which == 53) { //5 transitions tab
+                    _gaq.push(['_trackEvent', 'Hotkey', 'VideoTab']);
                     if (jQuery("#mediapicker-infobox").attr('class') == "infobox-visible") {
                         jQuery("#mediapicker-infobox").toggleClass("mediapicker-visible");
                         jQuery("#mediapicker-infobox").toggleClass("infobox-visible");
                     }
                     jQuery("#transition-tab").click();
                 } else if ( event.which == 54) { //6 text tab
+                    _gaq.push(['_trackEvent', 'Hotkey', 'TextTab']);
                     if (jQuery("#mediapicker-infobox").attr('class') == "infobox-visible") {
                         jQuery("#mediapicker-infobox").toggleClass("mediapicker-visible");
                         jQuery("#mediapicker-infobox").toggleClass("infobox-visible");
                     }
                     jQuery("#text-tab").click();
                 } else if ( event.which == 112 ) { //P - focus current clip
+                    _gaq.push(['_trackEvent', 'Hotkey', 'FocusCurrent']);
                     var offset = jQuery(".timeline-video-clips").children(".selected").css('left');
                     jQuery(".editor-timeline").scrollLeft(offset.substring(0,offset.lastIndexOf("px")));
                 } else if ( event.which == 91 ) { // [ - focus start
+                    _gaq.push(['_trackEvent', 'Hotkey', 'FocusStart']);
                     jQuery(".editor-timeline").scrollLeft(0);
                 } else if ( event.which == 93 ) { // ] - focus end
+                    _gaq.push(['_trackEvent', 'Hotkey', 'FocusEnd']);
                     var offset = jQuery(".timeline-video-clips").children().last().css('left');
                     jQuery(".editor-timeline").scrollLeft(offset.substring(0,offset.lastIndexOf("px")));
                 } else if ( event.which == 104 ) { // H - show help
+                    _gaq.push(['_trackEvent', 'Hotkey', 'ToggleHelp']);
                     $("#yteditorpro_help").modal('toggle');
                 }
             } 
