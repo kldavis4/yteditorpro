8c6a7a2429c07a6d633838688dc50bb121d6db87
Kelly Davis <kldavis4@gmail.com>
2016-03-10 18:01:15 -0500

    Extracted analytics id to a separate settings.json file

--- js/gahelper.js
+++ js/gahelper.js
@@ -1,15 +1,18 @@
 //Google Analytics Setup
 //
 var _gaq = _gaq || [];
-_gaq.push(function() {
-    _gaq.push(['_setAccount', 'UA-10393243-10']);
-});
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+jQuery.getJSON('settings.json',function(settings) {
+    _gaq.push(function() {
+        _gaq.push(['_setAccount', settings.ga_tracker_id]);
+    });
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = 'https://ssl.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+});
 
 chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
     _gaq.push(msg.event);
