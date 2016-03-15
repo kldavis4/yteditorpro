86c1590e7d20bc267005d457fcbfd7b2be165c7c
Kelly Davis <kldavis4@gmail.com>
2016-03-15 11:15:10 -0400

    Externalized mouse event simulation function

--- js/yteditorpro.js
+++ js/yteditorpro.js
@@ -49,70 +49,6 @@
     _gaq.push(['_trackPageview']);
     _gaq.push(['_trackEvent', 'Extension', 'GALoaded']);
 
-    //Event simulation
-    //http://stackoverflow.com/a/6158050/290918
-    //
-    function simulate(element, eventName)
-    {
-        var options = extend(defaultOptions, arguments[2] || {});
-        var oEvent, eventType = null;
-
-        for (var name in eventMatchers)
-        {
-            if (eventMatchers[name].test(eventName)) { eventType = name; break; }
-        }
-
-        if (!eventType)
-            throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
-
-        if (document.createEvent)
-        {
-            oEvent = document.createEvent(eventType);
-            if (eventType == 'HTMLEvents')
-            {
-                oEvent.initEvent(eventName, options.bubbles, options.cancelable);
-            }
-            else
-            {
-                oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
-                options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
-                options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
-            }
-            element.dispatchEvent(oEvent);
-        }
-        else
-        {
-            options.clientX = options.pointerX;
-            options.clientY = options.pointerY;
-            var evt = document.createEventObject();
-            oEvent = extend(evt, options);
-            element.fireEvent('on' + eventName, oEvent);
-        }
-        return element;
-    }
-
-    function extend(destination, source) {
-        for (var property in source)
-          destination[property] = source[property];
-        return destination;
-    }
-
-    var eventMatchers = {
-        'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
-        'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
-    }
-    var defaultOptions = {
-        pointerX: 0,
-        pointerY: 0,
-        button: 0,
-        ctrlKey: false,
-        altKey: false,
-        shiftKey: false,
-        metaKey: false,
-        bubbles: true,
-        cancelable: true
-    }
-
     //Receives for messages for swf player events
     window.addEventListener("message", function(event) {
         if (!active) return;
@@ -335,7 +271,6 @@
         }
     });
 
-    //$(".editor-timeline").before("<br /><div id='playpause'>PLAY</div><div id='timeline-scrubber'></div><br />");
     $("#storyboard").before("<br /><table id='controls_table'><tr><td><div id='pauseplay_button' class='playbutton'></div></td><td id='td_scrubber'><div id='timeline-scrubber'></div></td></tr></table>");
     $("#pauseplay_button").click(function(evt) {
         togglePlay();
@@ -538,15 +473,15 @@
                         if (timeSpan && timeSpan.length > 0) {
                             var startTime = parseClipTime(timeSpan[0].textContent);
                             var moveX = 0;
-                            simulate(handle[0], "mousedown", { pointerX: moveX, pointerY: 0 });
+                            syn.simulate(handle[0], "mousedown", { pointerX: moveX, pointerY: 0 });
                             while (true) {
                                 moveX += 1;
-                                simulate(handle[0], "mousemove", { pointerX: moveX, pointerY: 0 });
+                                syn.simulate(handle[0], "mousemove", { pointerX: moveX, pointerY: 0 });
                                 if ((parseClipTime(timeSpan[0].textContent) - startTime)>= 1000 || moveX > 1000) {
                                     break;
                                 }
                             }
-                            simulate(handle[0], "mouseup");
+                            syn.simulate(handle[0], "mouseup");
                         }
                     }
                 } else if ( event.which == 45) { //- key decrease by one second selected clip
@@ -559,15 +494,15 @@
                             if ( currentTimeLabel !== "0:01" && currentTimeLabel !== "0:00" ) {
                                 var startTime = parseClipTime(currentTimeLabel);
                                 var moveX = 1000;
-                                simulate(handle[0], "mousedown", { pointerX: moveX, pointerY: 0 });
+                                syn.simulate(handle[0], "mousedown", { pointerX: moveX, pointerY: 0 });
                                 while (true) {
                                     moveX -= 1;
-                                    simulate(handle[0], "mousemove", { pointerX: moveX, pointerY: 0 });
+                                    syn.simulate(handle[0], "mousemove", { pointerX: moveX, pointerY: 0 });
                                     if ((startTime - parseClipTime(timeSpan[0].textContent)) >= 1000 || moveX <= 0) {
                                         break;
                                     }
                                 }
-                                simulate(handle[0], "mouseup");
+                                syn.simulate(handle[0], "mouseup");
                             }
                         }
                     }
